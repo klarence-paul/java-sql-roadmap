fcd673add9cba87cdf1676add173c43f229707f8
Klarence <klarencepaulsantander@gmail.com>
2026-07-03 23:24:21 +0800

    Day 1: manual compile/run, Scanner+printf, integer overflow demo

--- day01/src/com/klarence/day1/Main.java
+++ day01/src/com/klarence/day1/Main.java
@@ -1,7 +1,30 @@
 package com.klarence.day1;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        Scanner scanner = new Scanner(System.in);
+
+        int maxie = Integer.MAX_VALUE;
+
+        System.out.println(maxie);
+
+        // This cause an overflow error and wraps around to the minimum value of the integer type
+        System.out.println(maxie + 1);
+
+        
+
+
+        System.out.print("Enter your name: ");
+        String name = scanner.nextLine();
+
+        System.out.print("Enter your age: ");
+        int age = scanner.nextInt();
+
+        System.out.print("Enter your favorite number: ");
+        int favNumber = scanner.nextInt();
+
+        System.out.printf("Your name is %s and you are %d years old and your favorite number is %d.", name, age, favNumber);
+        scanner.close();
     }
 }
