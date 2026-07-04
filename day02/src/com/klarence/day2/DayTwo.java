1627811c261266905af932ec408d11a403852010
Klarence <klarencepaulsantander@gmail.com>
2026-07-04 10:00:04 +0800

    Day 2 part1 opperations

---
+++ day02/src/com/klarence/day2/DayTwo.java
@@ -0,0 +1,15 @@
+package com.klarence.day2;
+
+public class DayTwo {
+    public static void main(String[] args){
+
+         int a = 10;
+         int b = 3;
+
+         System.out.println(a/b);
+         System.out.println(a%b);
+         System.out.println(a==b);
+         System.out.println(a!=b);
+    }
+
+} 
---
+++ day02/src/com/klarence/day2/DayTwop2.java
@@ -0,0 +1,24 @@
+package com.klarence.day2;
+
+import java.util.Scanner;
+
+public class DayTwop2 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int a = 0;
+
+        System.out.print("Enter your Number: ");
+        a = scanner.nextInt();
+
+        int b = a % 2;
+
+        if (b == 0) {
+            System.out.println(a + " The number is even.");
+        }
+        else {
+            System.out.println(a + " The number is odd.");
+        }
+
+        scanner.close();
+    }
+}
