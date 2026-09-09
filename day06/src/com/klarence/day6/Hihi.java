f14a44b754ea1f7cd442532658af63261375889d
Klarence <klarencepaulsantander@gmail.com>
2026-09-09 20:03:53 +0800

    String methods

---
+++ day06/src/com/klarence/day6/Hihi.java
@@ -0,0 +1,27 @@
+package com.klarence.day6;
+
+
+public class Hihi {
+    public static void main(String[] args) {
+        String name = "Klarence";
+        name.toUpperCase();   
+        
+        System.out.println(name);     
+
+        String text = "Hello, Klarence";
+
+       // System.out.println(text.length());
+      // System.out.println(text.trim());
+      // System.out.println(text.toUpperCase());
+     // System.out.println(text.toLowerCase());
+     // System.out.println(text.indexOf("Klarence"));
+      // System.out.println(text.substring(2,7));
+      System.out.println(text.replace("Klarence", "World"));
+
+
+
+
+
+
+    }
+}
